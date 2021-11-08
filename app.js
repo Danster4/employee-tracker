f89d51e7579fe236228299cbe5f0c43afa160023
Danster4 <86613317+Danster4@users.noreply.github.com>
2021-11-08 12:53:08 -0500

    connected inquirer and MySQL database to make a db query for all roles, departments, and employees

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
+const roleRoutes = require('./routes/apiRoutes/roleRoutes')
 const cTable = require('console.table');
-
+const db = require('./db/connection');
 
 
 
@@ -11,34 +12,49 @@ const startMenuQuestion = [
     message: 'What would you like to do?',
     choices: [
       "Show all Roles",
-      "Yes, please add an Intern to my team",
-      "No, there are no more team members to add"
+      "Show all Departments",
+      "Show all Employees"
     ]
   }
-
 ]
 
+
+const startMenu = async() => {
+  const result = await inquirer.prompt(startMenuQuestion)
+  .then(function(result) {
+    switch (result.startMenuQuestion) {
+      case "Show all Roles":
+        db.query('SELECT role.*, department.name AS department_name FROM role LEFT JOIN department ON role.department_id = department.id', function (err, results) {
+          console.log(results);
+        });
+        startMenu();
+        break;
+
+      case "Show all Departments":
+        db.query('SELECT * FROM department', function (err, results) {
+          console.log(results);
+        });
+        startMenu();
+        break;
+
+      case "Show all Employees":
+        db.query('SELECT * FROM employee', function (err, results) {
+          console.log(results);
+        });
+        startMenu();
+        break;
+    }
+  });
+}
 
 const startApp = async() => {
   console.log('Welcome to the Employee Tracker!');
   console.log('Please choose an option below to get started:');
 
-  const result = await inquirer.prompt(startMenuQuestion)
-  .then(function(result) {
-    switch (result.startMenuQuestion) {
-      case "Show all Roles":
-        showAllRoles();
-        break;
-
-      case "Yes, please add an Intern to my team":
-        // insert function;
-        break;
-
-      case "No, there are no more team members to add":
-        // insert function;
-        break;
-    }
-  })
+  startMenu();
 }
+
+  
+
 
 startApp();
