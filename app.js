985c5a94b98d15859dd172c30acd78f6a27ea6af
Danster4 <86613317+Danster4@users.noreply.github.com>
2021-11-09 10:52:16 -0500

    removed unnecessary server functionality from app, added console.table to make information look cleaner

--- app.js
+++ app.js
@@ -25,27 +25,32 @@ const startMenu = async() => {
     switch (result.startMenuQuestion) {
       case "Show all Roles":
         db.query('SELECT role.*, department.name AS department_name FROM role LEFT JOIN department ON role.department_id = department.id', function (err, results) {
-          console.log(results);
+          console.log("");
+          console.table(results);
         });
         startMenu();
         break;
 
       case "Show all Departments":
         db.query('SELECT * FROM department', function (err, results) {
-          console.log(results);
+          console.log("");
+          console.table(results);
         });
         startMenu();
         break;
 
       case "Show all Employees":
-        db.query('SELECT * FROM employee', function (err, results) {
-          console.log(results);
+        db.query('SELECT employee.*, role.title AS role_title FROM employee LEFT JOIN role ON employee.role_id = role.id', function (err, results) {
+          console.log("");
+          console.table(results);
         });
         startMenu();
         break;
     }
   });
 }
+
+// employee.last_name AS manager_last_name FROM employee LEFT JOIN employee ON employee.manager_id = employee.id
 
 const startApp = async() => {
   console.log('Welcome to the Employee Tracker!');
